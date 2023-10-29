804c40c604f9e9fe890bb33f8d086d64dc0774f2
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-10-30 02:10:16 +0530

    Added Auth

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -4,7 +4,7 @@ import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/action';
+import { createInvoice } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -4,7 +4,7 @@ import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/action';
+import { updateInvoice } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({ invoice, customers }: { invoice: InvoiceForm; customers: CustomerField[] }) {
@@ -25,7 +25,7 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
             <select
               id="customer"
               name="customerId"
-              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
               aria-describedby="customer-error">
               <option value="" disabled>
@@ -62,7 +62,7 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
                 type="number"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block cursor-pointer w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
