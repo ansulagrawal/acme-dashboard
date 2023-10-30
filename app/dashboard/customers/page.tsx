d5b6b6a8f2e150c5758f9d02b437f331318186d7
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-10-30 22:23:13 +0530

    Added Customer Page and metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,19 @@
-export default function Customers() {
-    return <p>Customers Page</p>;
-  }
-  
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function CustomerPage({ searchParams }: { searchParams?: { query?: string; page?: string } }) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
